61cf35d4c58c57f0a71a5e72a4b08af5d5fce3be
aminacherop <cheropamina26@gmail.com>
2025-03-06 18:59:03 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+
+function saturdayFun( activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+console.log(saturdayFun());
+
+
+const mondayWork = function(activity ="go to the office"){
+    return `This Monday, I will ${activity}.`
+
+}
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`;
+
+    }
+}
+const result1 = wrapAdjective("%")("a dedicated programmer");
+const result2 = wrapAdjective("*")("a hard worker")
+
+
+console.log(result1);
+console.log(result2)
